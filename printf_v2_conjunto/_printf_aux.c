b85b6c98d894bbf00c4fa7958f6b1a68e04e4f1e
Ivan Hansen <ihk225@gmail.com>
2022-07-08 11:03:28 -0300

    A

--- printf_v2_conjunto/_printf_aux.c
+++ printf_v2_conjunto/_printf_aux.c
@@ -35,3 +35,46 @@ int stringiterator(va_list ap, int mode)
     va_end(ap);
     return (_strlen(s));
 }
+
+int ntostring(va_list ap, int base)
+{
+    char *buffer;
+    int n;
+    int i = 0, len, flag;
+
+    n = (va_arg(ap, int));
+    buffer = malloc(64);
+    flag = ((n < 0) ? 1 : 0);
+    n = ((n < 0) ? (n * -1) : n);
+    if (n != 0)
+    {
+        while (n > 0)
+        {
+            if (base <= 10)
+                buffer[i++] = ((n % base) + '0');
+            else if (base == 16)
+            {
+                if ((n % base) >= 10)
+                    buffer[i++] = ((n % base) + ((base == 32) ? 55 : 87));
+                else
+                    buffer[i++] = ((n % base) + '0');
+            }
+            n = n / base;
+        }
+        if (flag && (base == 10))
+            buffer[i++] = 45;    
+        i = ((base == 2) ? i : i - 1);
+        for (; i >= 0; i--)
+            write(1, &buffer[i], 1);
+    }
+    else
+    {
+        buffer[i++] = '0';
+        write(1, &buffer[i], 1);
+        return (1);
+    }
+    len = _strlen(buffer);
+    free(buffer);
+    va_end(ap);
+    return (len);
+}
