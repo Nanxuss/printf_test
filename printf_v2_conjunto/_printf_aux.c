7439c8677b3a4367b540e0c2d9c0d1ac6243ef51
Ivan Hansen <ihk225@gmail.com>
2022-07-08 11:08:31 -0300

    A

--- printf_v2_conjunto/_printf_aux.c
+++ printf_v2_conjunto/_printf_aux.c
@@ -78,3 +78,50 @@ int ntostring(va_list ap, int base)
     va_end(ap);
     return (len);
 }
+
+int ftostring(va_list ap, int precision)
+{
+    char *buffer;
+    double n;
+    int i, ppoint, len, mov, apoint;
+
+    n = (va_arg(ap, double));
+    mov = 1;
+    for (i = 0; i < precision; i++)
+        mov = 10 * mov;
+    buffer = malloc(64);
+    ppoint = (int) n;
+    apoint = (int)((n - ppoint) * mov);
+    i = 0;
+    if (n != 0)
+    {
+        while (apoint > 0)
+        {
+            buffer[i++] = ((apoint % 10) + '0');
+            apoint = apoint / 10;
+        }
+        buffer[i++] = 46;
+        while ((ppoint > 0) && (i < 63))
+        {
+            buffer[i++] = ((ppoint % 10) + '0');
+            ppoint = ppoint / 10;
+        }
+        if (n < 1)
+            buffer[i++] = '0';
+        for (; i >= 0; i--)
+            write(1, &buffer[i], 1);
+    }
+    else
+    {
+        buffer[i++] = '0';
+        buffer[i++] = '.';
+        for (i = 0; i == precision; i++)
+            buffer[i++] = '0';
+        write(1, buffer, (_strlen(buffer)));
+        return (1);
+    }
+    len = _strlen(buffer);
+    free(buffer);
+    va_end(ap);
+    return (len);
+} 
