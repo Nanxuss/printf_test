e21948e1da711f4d7e91a6338d12bac7a241c366
Ivan Hansen <ihk225@gmail.com>
2022-07-08 11:24:26 -0300

    A

--- printf_v2_conjunto/_printf.c
+++ printf_v2_conjunto/_printf.c
@@ -31,7 +31,7 @@ int _printf(const char *format, ...)
             else
             {
                 j += singlewrite(format[i++]);
-                i++;
+                i = ((format[i] == '%') ? (i + 1) : i);
             }
         }
     va_end(ap);
