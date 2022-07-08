1f67e7e0cac8c7ca7aedd06a9d2054dc33610f9b
Ivan Hansen <ihk225@gmail.com>
2022-07-08 10:29:02 -0300

    A

--- printf_v2_conjunto/_printf_func_finder.c
+++ printf_v2_conjunto/_printf_func_finder.c
@@ -5,11 +5,11 @@ ftype func_finder(char c)
     ftype func[] = {
         {'c', _putchar, 0},
         {'s', stringiterator, 0},
-        {0, 0, 0}
+        {'0', 0, 0}
     };
     int i = 0;
 
-    while (func[i].fmt != 0)
+    while (func[i].fmt != '0')
     {
         if (func[i].fmt == c)
             return func[i];
