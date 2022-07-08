a60a2061318724e247474c6136e0ddf98135f6fa
Ivan Hansen <ihk225@gmail.com>
2022-07-08 10:43:55 -0300

    A

--- printf_v2_conjunto/_printf.c
+++ printf_v2_conjunto/_printf.c
@@ -21,9 +21,17 @@ int _printf(const char *format, ...)
             if (format[i] == '%')
 			{
                 func_data = func_finder(format[i + 1]);
-			    j += (*(func_data.op))(ap, func_data.mode);
-			    i = ((func_data.fmt == '.') ? (i + 4) : (i + 2));
+			    if (func_data.op != 0)
+                {
+                    j += (*(func_data.op))(ap, func_data.mode);
+			        i = ((func_data.fmt == '.') ? (i + 4) : (i + 2));
+                }
             }
+            else
+                {
+                    j += write(1, &format[i], 1);
+                    i++;
+                }
         }
     va_end(ap);
     return (j);
